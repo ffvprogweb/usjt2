79a5f307866f54cc4e9c2264faa6a9ebd0151583
probook <engsoftware1@gmail.com>
2020-11-09 21:35:57 -0200

    Inclusão da frase teste de carga do contexto

--- scel/src/main/java/com/usjt/demo/ScelApplication.java
+++ scel/src/main/java/com/usjt/demo/ScelApplication.java
@@ -3,13 +3,14 @@ package com.usjt.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- * Obejtivo - carrgar o contexto
+ * Objetivo - carregar o contexto
  */
 @SpringBootApplication
 public class ScelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScelApplication.class, args);
+		System.out.println("teste de carga do contexto");
 	}
 
 }
