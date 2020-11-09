4aae53639944dc64f7a5d6df957520d873a228f9
probook <engsoftware1@gmail.com>
2020-11-09 21:21:41 -0200

    Inclusão de comentario na classe ScelApplication

--- scel/src/main/java/com/usjt/demo/ScelApplication.java
+++ scel/src/main/java/com/usjt/demo/ScelApplication.java
@@ -2,7 +2,9 @@ package com.usjt.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Obejtivo - carrgar o contexto
+ */
 @SpringBootApplication
 public class ScelApplication {
 
