0ff0a3c1744057879f38e944e900abe5c0b233fa
probook <engsoftware1@gmail.com>
2020-11-09 21:37:48 -0200

    Revert "Inclusão da frase teste de carga do contexto"
    This reverts commit 79a5f307866f54cc4e9c2264faa6a9ebd0151583.

--- scel/src/main/java/com/usjt/demo/ScelApplication.java
+++ scel/src/main/java/com/usjt/demo/ScelApplication.java
@@ -3,14 +3,13 @@ package com.usjt.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- * Objetivo - carregar o contexto
+ * Obejtivo - carrgar o contexto
  */
 @SpringBootApplication
 public class ScelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScelApplication.class, args);
-		System.out.println("teste de carga do contexto");
 	}
 
 }
